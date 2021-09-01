8b3348dd0f23dda3cfe95c16d6bb0c04e8b843d3
Daniel <danielbb2000@gmail.com>
2021-09-01 16:36:09 -0300

    Finished module 2

--- App.js
+++ App.js
@@ -1,21 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { AuthContext } from './context';
 
-export default function App() {
+import { SignIn, CreateAccount, Profile, Home, Search, Details, Search2, Splash } from './Screen';
+
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen name="SignIn" component={SignIn} options={{ title: "Sign In" }} />
+    <AuthStack.Screen name="CreateAccount" component={CreateAccount} options={{ title: "Create Account" }} />
+  </AuthStack.Navigator>
+)
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={Home} />
+    <HomeStack.Screen name="Details" component={Details} options={({ route }) => ({
+      title: route.params.name
+    })} />
+  </HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search2" component={Search2} />
+  </SearchStack.Navigator>
+)
+const ProfileStack = createStackNavigator();
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name="Profile" component={Profile} />
+  </ProfileStack.Navigator>
+);
+
+const TabsSceen = () => (
+  <Tabs.Navigator>
+    <Tabs.Screen name="Home" component={HomeStackScreen} options={{ headerShown: false }} />
+    <Tabs.Screen name="Search" component={SearchStackScreen} options={{ headerShown: false }} />
+  </Tabs.Navigator>
+);
+const Drawer = createDrawerNavigator();
+const DrawerScreen = () => (
+  <Drawer.Navigator>
+
+    <Drawer.Screen name="Home" component={TabsSceen} />
+    <Drawer.Screen name="Profile" component={ProfileStackScreen} />
+  </Drawer.Navigator>
+);
+
+const RootStack = createStackNavigator();
+
+const RootStackScreen = ({ userToken }) => (
+  <RootStack.Navigator screenOptions={{
+    headerShown: false
+  }}>
+    {userToken ? <RootStack.Screen name="Auth" component={AuthStackScreen} options = {{ animationEnabled: false}}/>
+      : <RootStack.Screen name="App" component={DrawerScreen} options = {{ animationEnabled: false}}/>}
+
+
+  </RootStack.Navigator>
+);
+
+export default () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(null);
+  const authContext = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setIsLoading(false);
+        setIsLoggedIn('ufgruf');
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setIsLoggedIn("iuerfnui");
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setIsLoggedIn(null);
+      }
+    }
+  }, [])
+  React.useEffect(() => {
+    const time = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return function cleanUp() {
+      clearTimeout(time);
+    }
+  }, [])
+  if (isLoading) {
+    return <Splash />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <RootStackScreen userToken={isLoggedIn} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+      </NavigationContainer>
+    </AuthContext.Provider >
+  )
+}
